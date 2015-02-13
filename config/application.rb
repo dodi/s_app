b84c89c93403eb8fb22df855fd4bfacf82ebb881
dodi <dodisabaruddin@gmail.com>
2015-02-14 01:42:31 +0700

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module SApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.serve_static_assets = true
   end
 end
